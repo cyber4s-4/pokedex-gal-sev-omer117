dcda406a32b7f644788529b6787a85f6c0130242
omer117 <omer117.cyber4s.dev>
2022-06-29 19:53:07 +0300

    pt 1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
-fetch('https://pokeapi.co/docs/v2')
+
+let pokeEntriesData = fetch('https://pokeapi.co/api/v2/pokedex/1')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => handleData(data));
+
+let pokemonArr: Pokemon[] = [];
+
+function handleData(data: any) {
+    let pokeEntriesData = data.pokemon_entries;
+    console.log(pokeEntriesData);
+    for (let i = 0; i < pokeEntriesData.length; i++) {
+        console.log(pokeEntriesData[i]);
+        pokemonArr.push(pokeEntriesData[i])
+
+    }
+}
+
+
+
+class Pokemon {
+    id;
+    name;
+    url;
+
+    constructor(id: number, name: string, url: string) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        // this.generation = generation;
+
+    }
+}
+
